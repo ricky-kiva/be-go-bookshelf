f8efe1565a818683394803be7e63d02972c56c49
ricky-kiva <ricky.kiva@gmail.com>
2023-12-02 23:27:06 +0700

    feat(main): add main function

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gin-gonic/gin v1.9.1
 	github.com/jinzhu/gorm v1.9.16
 	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.1.1
 )
 
 require (
--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"be-go-bookshelf/app"
+	"be-go-bookshelf/db"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	db := db.InitDB()
+
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+
+	handler := app.New(db)
+
+	r.GET("/books", handler.GetBooks)
+
+	r.Run()
 }
